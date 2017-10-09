e74de4b3698611f70327d250f33bbcffc215dc1f
Rathil <rathilvasani@gmail.com>
2017-10-09 11:18:28 +0530

    pehchan 2.0

---
+++ pehchan2.0/header.php
@@ -0,0 +1,15 @@
+<!--Import Google Icon Font-->
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+       <!-- Compiled and minified CSS -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
+
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+       <!--Import jQuery before materialize.js-->
+      <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+
+  <!-- Compiled and minified JavaScript -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
+          
+          <script type="text/javascript" src="script.js"></script>
---
+++ pehchan2.0/navbar.php
@@ -0,0 +1,10 @@
+<nav>
+    <div class="nav-wrapper">
+      <a href="#!" class="brand-logo ">Pehchan</a>
+      <ul class=" right hide-on-med-and-down">
+        <li><a href="sass.html">Home</a></li>
+        <li><a href="badges.html">Products</a></li>
+        <li class="active"><a href="collapsible.html">Contact</a></li>
+      </ul>
+    </div>
+  </nav>
