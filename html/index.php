9f2288d820bdd84fa32a07c94126287c4b480fcb
unknown <rathilvasani@gmail.com>
2017-10-10 21:20:59 +0530

    done some changes

--- html/index.php
+++ html/index.php
@@ -4,14 +4,18 @@
 	<title>Pehchan</title>
 <?php require 'header.php' ?>
 <!-- jQuery library -->
-
+<!-- Compiled and minified CSS -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
+  <!-- Compiled and minified JavaScript -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
 	<!-- Add icon library -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-	<script src="js/script.js" type="text/javascript"></script>
 <!-- 	<link rel="stylesheet" type="text/css" href="../CSS/style.css">
- -->	<style type="text/css">
+ -->
+  <script src="js/script.js"></script>
+
+ 	<style type="text/css">
 		#subscribe{
 				background-color: grey;
 				color: white;
@@ -32,33 +36,30 @@
 		}
 
 	</style>
-
 </head>
 <body >
 
 
-<!-- <div style="position: top right; width: 100% background-color:#f2f2f2;">
-<img src="../Images/pehchan.gif" style="height:100px;width:280px;">
 
-</div> -->
+</div>
 <?php require 'navbar.php' ?>
-<div class="carousel carousel-slider center" data-indicators="true">
+   <div class="carousel carousel-slider center" data-indicators="true">
     <div class="carousel-fixed-item center">
       <a class="btn waves-effect white grey-text darken-text-2">button</a>
     </div>
-    <div class="carousel-item red white-text" href="#one!" style="background:url('https://lorempixel.com/800/400/food/1') center center no-repeat;background-size:cover;">
+    <div class="carousel-item red white-text" href="#one!">
       <h2>First Panel</h2>
       <p class="white-text">This is your first panel</p>
     </div>
-    <div class="carousel-item amber white-text" href="#two!" style="background:url('https://lorempixel.com/800/400/food/2') center center no-repeat;background-size:cover;">
+    <div class="carousel-item amber white-text" href="#two!">
       <h2>Second Panel</h2>
       <p class="white-text">This is your second panel</p>
     </div>
-    <div class="carousel-item green white-text" href="#three!" style="background:url('https://lorempixel.com/800/400/food/3') center center no-repeat;background-size:cover;">
+    <div class="carousel-item green white-text" href="#three!">
       <h2>Third Panel</h2>
       <p class="white-text">This is your third panel</p>
     </div>
-    <div class="carousel-item blue white-text" href="#four!" style="background:url('https://lorempixel.com/800/400/food/4') center center no-repeat;background-size:cover;">
+    <div class="carousel-item blue white-text" href="#four!">
       <h2>Fourth Panel</h2>
       <p class="white-text">This is your fourth panel</p>
     </div>
@@ -69,50 +70,18 @@
   <div class="parallax-container">
     <div class="parallax"><img src="../Images/para1.jpg"></div>
   </div>
-  <div class="section white">
+  <div class="section black">
     <div class="row container">
-      <h2 class="header">Parallax</h2>
+      <h2 class="header white-text">Parallax</h2>
       <p class="grey-text text-darken-3 lighten-3">Parallax is an effect where the background content or image in this case, is moved at a different speed than the foreground content while scrolling.</p>
     </div>
   </div>
   <div class="parallax-container">
     <div class="parallax"><img src="../Images/para2.jpg"></div>
   </div>
-<!-- parallax  -->
-<div class="parallax"></div>
-<div class="block1" style="height: 400px; width: 100%; ">
-	<div class="row">
-		<div class="col-md-offset-1 black">
-			<span class="h3 black-text">ABOUT US:</span>
-<p>Pehchan is one of the leading gifting firms in the corporate field. In a span of 8 progressive years,we have looked back only to see the smiling faces of our satisfied clients. At Pehchan, we strive at making 'customization' to gifting what 'touch screen' is to mobile phones.</p>
-<br>
-
-<span class="h3">VISION AND MISSION:</span>
-<p style="padding-top: -20px;">To make sure  a Corporate or Commercial Entity makes only Customized for the internal and external, Branding, Promotion or Advertising purpose.<br>
-Customization is not and should be limited to limited privileged few, it should be a necessity which should be enjoyed and owned by all.<br>
-The Pehchan brand believes in providing high quality products that everybody in the corporate world would aspire to own.<br>
-Pehchan provides great items that have been developed with considerable thought and Customization keeping the new Age Companies and Industries need for such products at the forefront of the process.
-<br></p>
-<br>
-<span class="h3"><b>WE SPECIALIZE IN MANUFACTURING CUSTOMISED & PERSONALISED T-SHIRTS</b> </span><br>
-					      <span class="h5">We specialize in customization through varied processes:</span><br>
-					      <span class="h5"><b>Printing:</b></span>
-					      <p>Our Specialize Printing unit provides excellent customized corporate gifts.We understand customer needs when we customize the gifts, be it offset printing,plastic soil printing,digital transfer printing,multi-colour stickers,screen printing,pigment printing or any other form of printing</p>
-					      <span class="h5"><b>Embroidery:</b></span>
-					      <p>Lattest com,puterised machines with high speeds are used to customize yuor shirt,T-shirts,caos and jackets to give you the ultimate embroidery and branding. </p>
-					      <span class="h5"><b> Engraving/Embrossing:</b></span>
-					      <p>Imported Computerized LAzer etching machines are used to personalize wooden,metal pens, desktop items,crystal momentos,wooden souvnirs & acrylic trophies are given a new look.</p>
-
-		</div>
-	</div>
-
-
-</div>
-
-
-<div class="parallax1"></div>
-<div class="block" style="min-height: px; width: 100%">
+<div class="block" style=" width: 100%">
 
 	<?php require 'footer.php' ?>
+	<?php require 'chat.php' ?>
 </body>
 </html>
--- html/js/script.js
+++ html/js/script.js
@@ -11,12 +11,12 @@ $(document).ready(function(){
  $(document).ready(function(){
       $('.carousel.carousel-slider').carousel({fullWidth: true});
     });
-autoplay()
-function autoplay() {
-    $('.carousel').carousel('next');
-    setTimeout(autoplay, 4500);
-}
+
 
 $(document).ready(function(){
      $('.parallax').parallax();
    });
+
+	$(document).ready(function(){
+      $('.carousel').carousel();
+    });
