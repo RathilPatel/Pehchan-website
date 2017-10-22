08b1bb0361478d11449d3a443c223dae0c93bce6
Rathil <rathilvasani@gmail.com>
2017-10-22 18:04:57 +0530

    some errors with register

--- html/contact.php
+++ html/contact.php
@@ -24,85 +24,91 @@
 
 				<div class="row">
 					<div class="input-field col s12">
-						<select>
+						<select name="Type">
 							<option value="" disabled selected>Choose Type of Client</option>
-							<option value="1">Vendor</option>
-							<option value="2">Customer</option>
+							<option value="Vendor">Vendor</option>
+							<option value="Customer">Customer</option>
 						</select>
 					</div>
 					<div class="input-field col s12">
-						<input id="first_name" type="text" class="validate">
-						<label for="first_name">First Name</label>
-					</div>
-					<div class="input-field col s12">
-						<input id="last_name" type="text" class="validate">
-						<label for="last_name">Last Name</label>
+						<input id="Name" type="text" name="Name" class="validate">
+						<label for="Name">Full Name</label>
 					</div>
 
 					<div class="input-field col s12">
-						<input id="email" type="email" class="validate">
-						<label for="email">Email</label>
+						<input id="Address" type="text" name="Address" class="validate">
+						<label for="Address">Address</label>
 					</div>
 
 					<div class="input-field col s12">
-						<input id="last_name" type="text" class="validate">
-						<label for="last_name">Address</label>
-					</div>
-
-					<div class="input-field col s12">
-						<input id="last_name" type="text" class="validate">
-						<label for="last_name">Pincode</label>
+						<input id="Pincode" type="text" name="Pincode" class="validate">
+						<label for="Pincode">Pincode</label>
 					</div>
 
 						<div class="input-field col s12">
-							<input id="last_name" type="text" class="validate">
-							<label for="last_name">City</label>
+							<input id="City" type="text" name="City" class="validate">
+							<label for="City">City</label>
 						</div>
 
 						<div class="input-field col s12">
-						<input id="last_name" type="text" class="validate">
-						<label for="last_name">GST No.</label>
+						<input id="GST" type="text" name="GST" class="validate">
+						<label for="GST">GST No.</label>
 						</div>
 
 					<div class="input-field col s12">
-					<input id="last_name" type="text" class="validate">
-					<label for="last_name">Type of Dealer</label>
+					<input id="Typeofdealer" type="text" name="Typeofdealer" class="validate">
+					<label for="Typeofdealer">Type of Dealer</label>
 					</div>
 
 					<div class="input-field col s12">
-					<input id="last_name" type="text" class="validate">
-					<label for="last_name">Bank name</label>
+						<input id="PANNumber" type="text" name="PANNumber" class="validate">
+						<label for="PANNumber">PAN Number (if not registered under GST)</label>
 					</div>
 
 					<div class="input-field col s12">
-					<input id="last_name" type="text" class="validate">
-					<label for="last_name">Account number</label>
+						<input id="Contactperson" type="text" name="Contactperson" class="validate">
+						<label for="Contactperson">Contact Person</label>
 					</div>
 
 					<div class="input-field col s12">
-					<input id="last_name" type="text" class="validate">
-					<label for="last_name">Bank address</label>
+						<input id="contact" type="text" name="contact" class="validate">
+						<label for="contact">Contact</label>
 					</div>
 
 					<div class="input-field col s12">
-					<input id="last_name" type="text" class="validate">
-					<label for="last_name">IFSC number</label>
+						<input id="Email" type="email" name="Email" class="validate">
+						<label for="Email">Email</label>
+					</div>
+
+					<div class="input-field col s12">
+					<input id="Bankname" type="text" name="Bankname" class="validate">
+					<label for="Bankname">Bank name</label>
+					</div>
+
+					<div class="input-field col s12">
+					<input id="Accountno" type="text" name="Accountno" class="validate">
+					<label for="Accountno">Account number</label>
+					</div>
+
+					<div class="input-field col s12">
+					<input id="branchaddress" type="text" name="branchaddress" class="validate">
+					<label for="branchaddress">Bank address</label>
+					</div>
+
+					<div class="input-field col s12">
+					<input id="IFSC" type="text" name="IFSC" class="validate">
+					<label for="IFSC">IFSC number</label>
 					</div>
 				</div>
+				<input type="submit" name="Submit1" value="Submit" class="waves-effect waves-light btn"style="width:100%;" >
 
-
-
-				<div class="input-field col s12">
-					<textarea id="Message" class="materialize-textarea validate"></textarea>
-					 <label for="Message">Message</label>
-				</div>
 			</form>
 
 		 </div>
-		 <div class="modal-footer">
+		 <!-- <div class="modal-footer">
 			 <input type="submit" name="Submit1" value="Submit" class="waves-effect waves-light btn"style="width:100%;" >
-			 <!-- <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat ">Submit</a> -->
-		 </div>
+			 <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat ">Submit</a>
+		 </div> -->
 	 </div>
 
 		</div>
@@ -133,6 +139,7 @@
 				 <label for="Message">Message</label>
 			</div>
 			<input type="submit" name="Submit" value="Submit" class="waves-effect waves-light btn"style="width:100%;" >
+
 
     </form>
   </div>
--- html/registerclient.php
+++ html/registerclient.php
@@ -2,31 +2,48 @@
 
   require 'dbh.php';
 
-$Type=$_POST[''];
-$Name=$_POST[''];
-$Address=$_POST[''];
-$Pincode=$_POST[''];
-$City=$_POST[''];
-$Typeofdealer=$_POST[''];
-$PANNumber=$_POST[''];
-$Contactperson=$_POST[''];
-$contact=$_POST[''];
-$Email=$_POST[''];
-$Bankname=$_POST[''];
-$Accountno=$_POST[''];
-$branchaddress=$_POST[''];
-$IFSC=$_POST[''];
-if (empty($fname) ||empty($lname) || empty($email) || empty($message)) {
-  echo "values not input";
-  exit();
-}
-else {
+$Type=$_POST['Type'];
+$Name=$_POST['Name'];
+$Address=$_POST['Address'];
+$Pincode=$_POST['Pincode'];
+$City=$_POST['City'];
+$Typeofdealer=$_POST['Typeofdealer'];
+$PANNumber=$_POST['PANNumber'];
+$Contactperson=$_POST['Contactperson'];
+$contact=$_POST['contact'];
+$Email=$_POST['Email'];
+$Bankname=$_POST['Bankname'];
+$Accountno=$_POST['Accountno'];
+$branchaddress=$_POST['branchaddress'];
+$IFSC=$_POST['IFSC'];
+
+// $Type='Type';
+// $Name='Name';
+// $Address='Address';
+// $Pincode=546;
+// $City='City';
+// $Typeofdealer='Typeofdealer';
+// $PANNumber='PANNumber';
+// $Contactperson='Contactperson';
+// $contact='contact';
+// $Email='Email';
+// $Bankname='Bankname';
+// $Accountno=5161651;
+// $branchaddress='branchaddress';
+// $IFSC='IFSC';
+
+
   $sql= "INSERT into Client(Type,Name,Address,Pincode,City,Typeofdealer,PANNumber,Contactperson,contact,Email,Bankname,Accountno,branchaddress,IFSC)
-  values ($Type,$Name,$Address,$Pincode,$City,$Typeofdealer,$PANNumber,$Contactperson,$contact,$Email,$Bankname,$Accountno,$branchaddress,$IFSC);";
+  values ('$Type','$Name','$Address','$Pincode','$City','$Typeofdealer','$PANNumber','$Contactperson','$contact','$Email','$Bankname','$Accountno','$branchaddress','$IFSC');";
   mysqli_query($conn,$sql);
-  echo " successs";
+  if(mysqli_query($conn,$sql)>1){
+        echo " Fail";
+  }
+  else {
+    echo "success";
+  }
+mysql_close($conn);
   exit();
-}
 
 
 ?>
