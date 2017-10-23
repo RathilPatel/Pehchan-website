544a0be02d5951a947acb72cf4e9e2603c38a1de
Rathil <rathilvasani@gmail.com>
2017-10-23 13:49:45 +0530

    gallery something update

---
+++ html/Products/keychain.php
@@ -0,0 +1,23 @@
+<?php
+
+  $dir = glob('../../pehchan/Corporate gifting/Keychain/{*.jpg,*.png,*.gif}',GLOB_BRACE);
+?>
+<div class="row">
+<div class="col s10 offset-s1">
+  <div class="row">
+<?php
+  foreach ($dir as $value) {
+    ?>
+    <div class="col s2" >
+      <a href="<?php echo $value;?>" class="lightBox">
+        <img src="<?php echo $value; ?>" alt="<?php echo $value; ?>" style="height:200px; width:153px; padding:5px 5px 5px 5px;" >
+      </a>
+
+    </div>
+
+
+    <?php
+  }
+  ?>
+
+<!-- ----------------------------------------------------------------------------------- -->
--- html/productnav.php
+++ html/productnav.php
@@ -12,46 +12,46 @@
  </nav>
 
  <ul id='tshirts' class='dropdown-content'>
-    <li><a href="round.php">Round Neck</a></li>
-    <li><a href="polo.php">Polo/Collar</a></li>
+    <li><a href="#" id="round">Round Neck</a></li>
+    <li><a href="#" id="polo">Polo/Collar</a></li>
   </ul>
 
   <ul id='cgifts' class='dropdown-content'>
-    <li><a href="frames.php">Frames</a></li>
-    <li><a href="crystal.php">Crystal Gifts</a></li>
-    <li><a href="leather.php">Leather Gifts</a></li>
-    <li><a href="#!">Apparels</a></li>
-    <li><a href="bag.php">Bags</a></li>
-    <li><a href="stationary">Office Stationary</a></li>
-    <li><a href="goldfoil">Gold Foil Article</a></li>
-    <li><a href="giftsets">Gift sets</a></li>
-    <li><a href="pen.php">Pens</a></li>
-    <li><a href="sipper.php">Sipper	Bottles</a></li>
-    <li><a href="keychain.php">Keychains</a></li>
-    <li><a href="desktopparticle.php">Desktop Article</a></li>
-    <li><a href="cg_misc.php">Miscellaneous</a></li>
+    <li><a href="#" id="frame">Frames</a></li>
+    <li><a href="#" id="crystal">Crystal Gifts</a></li>
+    <li><a href="#" id="leather">Leather Gifts</a></li>
+    <li><a href="#!" id="apparels">Apparels</a></li>
+    <li><a href="#" id="bag">Bags</a></li>
+    <li><a href="#" id="stationary">Office Stationary</a></li>
+    <li><a href="#" id="gold">Gold Foil Article</a></li>
+    <li><a href="#" id="gifts">Gift sets</a></li>
+    <li><a href="#" id="pen">Pens</a></li>
+    <li><a href="#" id="sipper">Sipper	Bottles</a></li>
+    <li><a href="#" id="keychain">Keychains</a></li>
+    <li><a href="#" id="desktop">Desktop Article</a></li>
+    <li><a href="#" id="cg_misc">Miscellaneous</a></li>
   </ul>
   <ul id='led' class='dropdown-content'>
-    <li><a href="led.php">LED Tshirts</a></li>
+    <li><a href="#" id="led">LED Tshirts</a></li>
 
   </ul>
   <ul id='snj' class='dropdown-content'>
-    <li><a href="sweatshirts.php">Sweatshirts</a></li>
-    <li><a href="jackets.php">Jackets</a></li>
-    <li><a href="hoodies.php">Hoodies</a></li>
+    <li><a href="#" id="sweatshirts">Sweatshirts</a></li>
+    <li><a href="#" id="jacket">Jackets</a></li>
+    <li><a href="#" id="hoodies">Hoodies</a></li>
   </ul>
 
   <ul id='it' class='dropdown-content'>
-    <li><a href="penddrives.php">Pendrives</a></li>
-    <li><a href="powerbanks.php">Powerbanks</a></li>
-    <li><a href="it_misc.php">Miscellaneous</a></li>
+    <li><a href="#" id="pendrive">Pendrives</a></li>
+    <li><a href="#" id="powerbank">Powerbanks</a></li>
+    <li><a href="#" id="it_misc">Miscellaneous</a></li>
   </ul>
 
   <ul id='uniform' class='dropdown-content'>
-    <li><a href="school.php">School Uniforms</a></li>
-    <li><a href="hospitality.php">Hospitality Uniforms</a></li>
-    <li><a href="formal.php">Formals</a></li>
-    <li><a href="security.php">Security Uniforms</a></li>
-    <li><a href="uni_misc.php">Miscellaneous</a></li>
+    <li><a href="#" id="school">School Uniforms</a></li>
+    <li><a href="#" id="hospital">Hospitality Uniforms</a></li>
+    <li><a href="#" id="formal">Formals</a></li>
+    <li><a href="#" id="security">Security Uniforms</a></li>
+    <li><a href="#" id="uni_misc">Miscellaneous</a></li>
 
   </ul>
