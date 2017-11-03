e87e1847948aa5232f9d02ea89d75a2059858fdc
Rathil <rathilvasani@gmail.com>
2017-11-03 12:38:10 +0530

    commit error with the collapsible

--- html/footer.php
+++ html/footer.php
@@ -1,80 +1,5 @@
 <?php require 'header.php';?>
 <footer class="page-footer">
-
-         <div class="container">
-           <ul class="collapsible popout" data-collapsible="accordion">
-             <li>
-               <div class="collapsible-header black-text center ">TSHIRTS</div>
-               <div class="collapsible-body">
-                 <span>
-                   Ready Stock for Plain T-shirts <br>
-                   Ready Stock for Plain shirts <br>
-                   Ready stock for Hoodies <br>
-                   Ready Stock for Kids T-shirts <br>
-                   Customised T-shirts <br>
-                   Sports T-shirts & Tracks <br>
-                   Pattern T-shirts on order <br>
-                   Hospitality Uniforms <br>
-                   Compressed T-shirts <br>
-                   Customized Caps <br>
-                   Corporate Uniforms <br>
-                   Blazers/Jackets /Suits <br>
-                   Complete Printing & Embroidery Facilities <br>
-                   MOQ as low as 25 pcs also accepted <br>
-                   LED T-shirts (Ready Stock & Custom Designs)
-                 </span>
-               </div>
-             </li>
-             <li>
-               <div class="collapsible-header black-text">CORPORATE GIFTING</div>
-               <div class="collapsible-body">
-                 <span>
-
-                   Frames <br>
-                   Crystals gifts <br>
-                   Leather gifts <br>
-                   Bags <br>
-                   Office Stationary <br>
-                   Pens <br>
-                   Gifts sets <br>
-                   Gold Foil Articles <br>
-                   Desktop Articles <br>
-                   Keychain <br>
-                   Sipper Bottles
-                 </span>
-               </div>
-             </li>
-             <li>
-               <div class="collapsible-header black-text">ELECTRONICS</div>
-               <div class="collapsible-body">
-                 <span>
-                   Pendrives <br>
-                   Powerbanks <br>
-                   Other IT Products
-
-                 </span>
-               </div>
-             </li>
-             <li>
-               <div class="collapsible-header black-text">UNIFORMS</div>
-               <div class="collapsible-body">
-                 <span>
-                   School Uniform
-                   Security Uniform
-                   Hospitality Uniform
-                   Formals
-                 </span>
-               </div>
-             </li>
-             <li>
-               <div class="collapsible-header black-text">BRAND CONSULTANCY</div>
-               <div class="collapsible-body">
-                 <span></span>
-               </div>
-             </li>
-           </ul>
-
-
            <div class="row">
 
              <div class="col l3 offset-l1 s12">
@@ -134,16 +59,4 @@
            </div>
          </div>
        </footer>
-       <!--Start of Tawk.to Script-->
-       <script type="text/javascript">
-       var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-       (function(){
-       var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-       s1.async=true;
-       s1.src='https://embed.tawk.to/59dcced0c28eca75e462535b/default';
-       s1.charset='UTF-8';
-       s1.setAttribute('crossorigin','*');
-       s0.parentNode.insertBefore(s1,s0);
-       })();
-       </script>
-       <!--End of Tawk.to Script-->
+  
