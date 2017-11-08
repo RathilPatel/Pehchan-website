0421164af28ad4b7898518e677f9868dcca8713e
unknown <rathilvasani@gmail.com>
2017-11-08 17:39:28 +0530

    changing saves and os boots

--- pehchan2.0/js/script.js
+++ pehchan2.0/js/script.js
@@ -2,5 +2,19 @@
     $(document).ready(function(){
       $('.parallax').parallax();
       $('.modal').modal();
+      $("#pushdown").hide();
     });
-        
+
+
+      var options = [
+        {selector: '#pushdown', offset:0, callback: function(e) {
+          $("#pushdown").fadeIn("slow");
+          )
+        } },
+        {selector: '#page1',offset:50,callback: function(e){
+        $("#page1").fadeIn("");  
+        }
+      },
+        {},
+      ];
+      Materialize.scrollFire(options);
