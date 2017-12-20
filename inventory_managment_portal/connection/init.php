82ec9595707c8ef6b369153444c3ff0ba2353ed0
Shyamal Makwana <makwanashyamal96@gmail.com>
2017-12-20 13:19:13 +0530

    Added session.php to store session into database.
    BUT NOT WORKING.

--- inventory_managment_portal/connection/init.php
+++ inventory_managment_portal/connection/init.php
@@ -1,4 +1,10 @@
 <?php
+    session_set_save_handler('_open',
+                         '_close',
+                         '_read',
+                         '_write',
+                         '_destroy',
+                         '_clean');
     session_start();
     //error_reporting(0);
     require 'connect.php';
