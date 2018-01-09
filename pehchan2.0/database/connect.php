836dcb253086523a841b3f2233a7e56b80fcb9b5
root <rathilvasani@gmail.com>
2018-01-09 23:15:34 +0530

    commiting database change

--- pehchan2.0/database/connect.php
+++ pehchan2.0/database/connect.php
@@ -1,5 +1,5 @@
 <?php
-$con = mysqli_connect("localhost","mypenynj","","Pehchan");
+$con = mysqli_connect("localhost","mypenynj","sundaY101","mypenynj_pehchan");
 if(!$con) {
     die("Connection Failed :" .mysqli_connect_error());
 }
