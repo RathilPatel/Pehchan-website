1687a9f49ce17e6624b1053549c717c7ad683af7
Rathil <rathilvasani@gmail.com>
2018-01-09 10:48:35 +0530

    commiting the changes for home page

--- pehchan2.0/html/index.php
+++ pehchan2.0/html/index.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <title>Pehchan-Corporate Gifting & Customised Tshirts</title>
-<!--    <meta name="viewport" content="width=device-width, initial-scale=1.0">-->
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
   <?php require '../headerandfooter/header.php'; ?>
   <style media="screen">
@@ -22,7 +22,7 @@
   </head>
   <body style="background-color:#e0e0e0;">
     <!-- <div class="hameid-loader-overlay"></div> -->
-    
+
     <div class="parallax-container" style="position:relative; height:400px;">
       <div class="parallax " style="position:absolute;">
         <img src="../images/10722.jpg">
@@ -47,7 +47,11 @@
                   <div class="black-text flow-text pageicondetail" style="align-item">
                     <h4 class="grey-text text-darken-3"> CUSTOMISED TSHIRTS AND UNIFORMS</h4>
                     <hr>
-                    <h5 class="grey-text text-darken-1 pageicondesc">LOREM IPSUM DOLOR SIT AMET, CONSECTETUER ADIPISCING ELIT. PRAESENT VESTIBULUM MOLE</h5>
+                    <h5 class="grey-text text-darken-1 pageicondesc">The identity of Pehchan is incomplete without the mention of t-shirts.
+From round to polo-necks and plain to printed, watch as we bring your
+design to life with our state of the art technology, extensive variety and
+an eye for detail making the most basic corporate T-shirts look great
+and feel greater.</h5>
                     <a href="#" class="btn transparent black-text" style="border-color:black; margin-bottom:10%; margin-top:10%; ">READ MORE</a>
                   </div>
 
@@ -62,7 +66,11 @@
                   <div class="black-text flow-text pageicondetail" style="align-text">
                     <h4 class="grey-text text-darken-3">PROMOTIONAL MERCHANDISE</h4>
                     <hr>
-                    <h5 class="grey-text text-darken-1 pageicondesc">LOREM IPSUM DOLOR SIT AMET, CONSECTETUER ADIPISCING ELIT. PRAESENT VESTIBULUM MOLESTIE LACUS. AENEAN NONUMMY</h5>
+                    <h5 class="grey-text text-darken-1 pageicondesc">Gone are the days when you could congratulate a friend on a new business
+with a box of laddoos . Now is the time you spend your money on something
+more practical and less edible. With a wide range of office stationery, fancy
+writing instruments, desktop articles and gift sets, you'll never run out of
+options.</h5>
   <a href="#" class="btn transparent black-text" style="border-color:black; margin-bottom:10%; margin-top:10%; ">READ MORE</a>
                   </div>
                 </div>
@@ -145,16 +153,19 @@
         <div class="col l5 offset-s1 s10 animated slideInRight">
 
           <div class="pagetiletitle">
-              <h4>Formals Attire</h4>
+              <h4>FORMAL ATTIRE</h4>
           </div>
 
           <hr>
           <div class="pagetiledesc flow-text">
-            <span>LOREM IPSUM DOLOR SIT AMET, CONSECTETUER ADIPISCING ELIT.
-               PRAESENT VESTIBULUM MOLESTIE LACUS.
-               AENEAN NONUMMY HENDRERIT MAURIS. PHASELLUS PORTA.
-                FUSCE VARIUS MI. LOREM IPSUM DOLOR SIT AMET,
-                 CONSECTETUER ADIPISCING ELIT. PRAESENT VESTIBULUM MOLESTIE
+            <span>With a view that you can achieve anything if you dress for it,
+we bring you a wide range of apparels like Shirts, suits, waist
+coat, trousers, neck ties, boiler suits amongst almost an endless
+range of Uniforms for different needs.
+Uniforms are an important part of an organization. Repay the
+members of your organization by giving them a sense of
+belongingness.
+We cater to all kinds of uniform needs.
                </span>
           </div>
         </div>
@@ -162,7 +173,7 @@
       <div class="row">
         <div class="col l5 offset-s1 s10 animated slideInLeft">
           <div class="pagetiletitle">
-              <h4>Formals Attire</h4>
+              <h4>CORPORATE GIFTING</h4>
           </div>
           <hr>
           <div class="pagetiledesc">
@@ -186,7 +197,7 @@
           <div class="pagetiletitle">
                 <h4>Formals Attire</h4>
           </div>
-          <hr>
+          <hr>  
           <div class="pagetiledesc">
             <span>LOREM IPSUM DOLOR SIT AMET, CONSECTETUER ADIPISCING ELIT.
                PRAESENT VESTIBULUM MOLESTIE LACUS.
@@ -199,6 +210,26 @@
         </div>
 
       </div>
+      <div class="row">
+        <div class="col l5 offset-s1 s10 animated slideInLeft">
+          <div class="pagetiletitle">
+              <h4>UNIFORMS</h4>
+          </div>
+          <hr>
+          <div class="pagetiledesc">
+            <span>LOREM IPSUM DOLOR SIT AMET, CONSECTETUER ADIPISCING ELIT.
+               PRAESENT VESTIBULUM MOLESTIE LACUS.
+               AENEAN NONUMMY HENDRERIT MAURIS. PHASELLUS PORTA.
+                FUSCE VARIUS MI. LOREM IPSUM DOLOR SIT AMET,
+                 CONSECTETUER ADIPISCING ELIT. PRAESENT VESTIBULUM MOLESTIE
+               </span>
+          </div>
+        </div>
+        <div class="col l7 hide-on-med-and-down">
+      <img src="../images/gift.jpg" alt="" style="max-width:100%; height:auto;  margin:-11px -15px -15px 0px;">
+        </div>
+      </div>
+
     </div>
     <div class="white" style="margin-top:1%; padding-top:4%">
         <?php require '../headerandfooter/footer.php'; ?>
