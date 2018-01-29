ab2d8cb85a86a98be5f4fe471aea2883d04dd7ee
root <makwanashyamal96@gmail.com>
2018-01-29 15:32:31 +0530

    dbh connection change

--- start/include/dbh.php
+++ start/include/dbh.php
@@ -1,5 +1,5 @@
 <?php
-$con = mysqli_connect("localhost","root","","Pehchan");
+$con = mysqli_connect("localhost","mypenynj_rathil","sundaY101","mypenynj_pehchan");
 
 if(!$con) {
     die("Connection Failed :" .mysqli_connect_error());
