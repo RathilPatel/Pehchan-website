bb39634ceae99353a229a4d2641ce9f31f8b27ce
root <makwanashyamal96@gmail.com>
2018-02-10 10:24:56 +0530

    Autocomplete working with serial number in the product code

--- start/getserial.php
+++ start/getserial.php
@@ -1,17 +1,13 @@
 <?php
 require 'include/dbh.php';
 // header('Content-Type: application/json');
-$q = intval($_GET['q']);
-$sql = "Select `curr_serial` from serial where product='$q'";
+$q = $_GET['q'];
+$sql = "Select * from `serial` where product='$q'";
 $result = mysqli_query($con,$sql);
-$row = mysqli_fetch_array($result);
-while($row=result->fetch_assoc()){
-  $res1= $row['curr_serial'];
-  // $res1= $res1 + 1;
-  echo $res1;
-}
+$row = mysqli_fetch_array($con);
+$row = $result->fetch_assoc();
+  $res1 =  $row['curr_serial'];
+  echo $res1 + 1;
 // echo $row;
-mysqli_close($con);
 
 console.log($row);
-?>
