09a543dad26610033f8d82b71ca2c9bfad8818ba
root <rathilvasani@gmail.com>
2018-02-18 23:51:26 +0530

    50% product updation working

--- pehchan2.0/html/specprod.php
+++ pehchan2.0/html/specprod.php
@@ -1,10 +1,6 @@
 <?php
 require '../database/connect.php';
-$cat = $_GET['category'];
-
-$sql= "SELECT * FROM webprod WEHRE category ='$cat'";
-$result = mysqli_query($con,$sql);
-$row = mysqli_fetch_array($con);
+   $cat = $_GET['category'];
 
 
  ?>
@@ -13,6 +9,10 @@ $row = mysqli_fetch_array($con);
   <head>
     <meta charset="utf-8">
     <title> <?php echo $cat; ?>- Pehchan </title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     <?php require '../headerandfooter/header.php'; ?>
 
   </head>
@@ -20,48 +20,35 @@ $row = mysqli_fetch_array($con);
     <?php require 'navbar.php'; ?>
     <?php require 'productnav.php';?>
 
-<div class="container">
-  <div class="row">
-    <div class="col l2 s13">
-          <span> <?php  echo $cat;?></span>
-    </div>
-    <div class="col l3" >
-      <div class="card" style="min-height:190px;">
-    <div class="card-image waves-effect waves-block waves-light">
-      <img class="activator" src="images/office.jpg">
-    </div>
-    <div class="card-content">
-      <span class="card-title activator grey-text text-darken-4">Title<i class="material-icons right">more_vert</i></span>
-    </div>
-    <div class="card-reveal">
-      <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
-      <p>Here is some more information about this product that is only revealed once clicked on.</p>
-    </div>
-  </div>
+    <div class="container">
+
+      <div class="row">
+        <div class="col-lg-12">
+         <h2 class="my-4 text-center"> <?php echo $cat; ?> </h2>
+       </div>
+       <?php
+       $cat = $_GET['category'];
+
+       $sql= "SELECT * from webprod";
+       $result = mysqli_query($con,$sql);
+       while($row = mysqli_fetch_array($result)){
+
+        ?>
+       <div class="col-lg-4 col-sm-6 text-center mb-4">
+         <img class="rounded-circle img-fluid d-block mx-auto" src="data:image;base64,'.$row[1]." alt=<?php echo $row[0];?>>
+         <h3> <?php echo $row[2]; ?>
+           <!-- <small>Job Title</small> -->
+         </h3>
+         <p><?php echo $row[3];  ?></p>
+       </div>
+       <?php
+
+     }
+     mysqli_close($con);
+     ?>
 
     </div>
-
-  </div>
-  <div class="row">
-    <?php while($row = $result->fetch_assoc()){ ?>
-    <div class="card">
-  <div class="card-image waves-effect waves-block waves-light">
-    <img class="activator" src="images/office.jpg">
-  </div>
-  <div class="card-content">
-    <span class="card-title activator grey-text text-darken-4"><?php echo $row['title'];?><i class="material-icons right">more_vert</i></span>
-  </div>
-  <div class="card-reveal">
-    <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
-    <p>Here is some more information about this product that is only revealed once clicked on.</p>
-  </div>
-</div>
-<?php } ?>
-    </div>
-here will be the images
-  </div>
-
-</div>
+<div>
 
   </body>
 </html>
